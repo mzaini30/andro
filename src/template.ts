6a9c5e64eb295e3ce7e49e44c008750bf6bf54a6
Zen <muhzaini30@gmail.com>
2022-08-14 14:25:52 +0800

    hapus start.io dari andro.json

--- src/template.ts
+++ src/template.ts
@@ -3,7 +3,6 @@ export default {
   judul: "Aplikasi",
   admobApplication: "ca-app-pub-3940256099942544~3347511713",
   admobBanner: "ca-app-pub-3940256099942544/6300978111",
-  startAds: "204848974",
   versi: 1,
   id: "com.user.app",
 };
