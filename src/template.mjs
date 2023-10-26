0b9f12908822d3c3000d7e7ac075adebc3d7fb5e
mzaini30 <muhzaini30@gmail.com>
2023-10-26 09:22:37 +0800

    template

--- src/template.mjs
+++ src/template.mjs
@@ -4,6 +4,7 @@ export default {
   admobApplication: "ca-app-pub-3940256099942544~3347511713",
   admobBanner: "ca-app-pub-3940256099942544/6300978111",
   admobOpen: "ca-app-pub-3940256099942544/3419835294",
+  admobReward: "ca-app-pub-3940256099942544/5224354917",
   versi: 1,
   id: "com.user.app",
   docs: "https://andro-docs.github.io/",
